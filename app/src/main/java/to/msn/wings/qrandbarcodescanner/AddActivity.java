dca4eb17766e60f67fb7c06e9002be4728308862
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-07-24 21:57:11 +0900

    Redefined the names of variables.

--- app/src/main/java/to/msn/wings/qrandbarcodescanner/AddActivity.java
+++ app/src/main/java/to/msn/wings/qrandbarcodescanner/AddActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText title_input, author_input, pages_input;
+    EditText payee_input, payer_input, payment_input;
     Button add_button;
 
     @Override
@@ -18,17 +18,17 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        title_input = findViewById(R.id.title_input);
-        author_input = findViewById(R.id.author_input);
-        pages_input = findViewById(R.id.pages_input);
+        payee_input = findViewById(R.id.payee_input);
+        payer_input = findViewById(R.id.payer_input);
+        payment_input = findViewById(R.id.payment_input);
         add_button = findViewById(R.id.add_button);
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
-                myDB.addBook(title_input.getText().toString().trim(),
-                        author_input.getText().toString().trim(),
-                        Integer.valueOf(pages_input.getText().toString().trim()));
+                myDB.addBook(payee_input.getText().toString().trim(),
+                        payer_input.getText().toString().trim(),
+                        Integer.valueOf(payment_input.getText().toString().trim()));
             }
         });
     }
