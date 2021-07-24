64b20a6f2edbba8c5016abc24ddd5881a5b223e6
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-07-24 22:09:22 +0900

    Renamed the name of DB and its column name;

--- app/src/main/java/to/msn/wings/qrandbarcodescanner/MyDatabaseHelper.java
+++ app/src/main/java/to/msn/wings/qrandbarcodescanner/MyDatabaseHelper.java
@@ -12,14 +12,14 @@ import androidx.annotation.Nullable;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private Context context;
-    private static final String DATABASE_NAME = "BookLibrary.db";
+    private static final String DATABASE_NAME = "PaymentHistory.db";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String TABLE_NAME = "my_library";
+    private static final String TABLE_NAME = "my_history";
     private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_TITLE = "book_title";
-    private static final String COLUMN_AUTHOR = "book_author";
-    private static final String COLUMN_PAGES = "book_pages";
+    private static final String COLUMN_PAYEE = "payee";
+    private static final String COLUMN_PAYER = "payer";
+    private static final String COLUMN_PAYMENT = "payment";
 
     MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -30,9 +30,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + TABLE_NAME +
                 " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_TITLE + " TEXT, " +
-                COLUMN_AUTHOR + " TEXT, " +
-                COLUMN_PAGES + " INTEGER);";
+                COLUMN_PAYEE + " TEXT, " +
+                COLUMN_PAYER + " TEXT, " +
+                COLUMN_PAYMENT + " INTEGER);";
         db.execSQL(query);
     }
     @Override
@@ -45,9 +45,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put(COLUMN_TITLE, title);
-        cv.put(COLUMN_AUTHOR, author);
-        cv.put(COLUMN_PAGES, pages);
+        cv.put(COLUMN_PAYEE, title);
+        cv.put(COLUMN_PAYER, author);
+        cv.put(COLUMN_PAYMENT, pages);
         long result = db.insert(TABLE_NAME,null, cv);
         if(result == -1){
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
@@ -70,9 +70,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     void updateData(String row_id, String title, String author, String pages){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(COLUMN_TITLE, title);
-        cv.put(COLUMN_AUTHOR, author);
-        cv.put(COLUMN_PAGES, pages);
+        cv.put(COLUMN_PAYEE, title);
+        cv.put(COLUMN_PAYER, author);
+        cv.put(COLUMN_PAYMENT, pages);
 
         long result = db.update(TABLE_NAME, cv, "_id=?", new String[]{row_id});
         if(result == -1){
