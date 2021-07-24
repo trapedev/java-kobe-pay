af04d1220bb88081fa20129f90105b626e0a67f0
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-07-24 22:16:27 +0900

    Renamed the name of variables;

--- app/src/main/java/to/msn/wings/qrandbarcodescanner/UpdateActivity.java
+++ app/src/main/java/to/msn/wings/qrandbarcodescanner/UpdateActivity.java
@@ -14,19 +14,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateActivity extends AppCompatActivity {
 
-    EditText title_input, author_input, pages_input;
+    EditText payee_input, payer_input, payment_input;
     Button update_button, delete_button;
 
-    String id, title, author, pages;
+    String id, payee, payer, payment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
 
-        title_input = findViewById(R.id.title_input2);
-        author_input = findViewById(R.id.author_input2);
-        pages_input = findViewById(R.id.pages_input2);
+        payee_input = findViewById(R.id.payee_input2);
+        payer_input = findViewById(R.id.payer_input2);
+        payment_input = findViewById(R.id.payment_input2);
         update_button = findViewById(R.id.update_button);
         delete_button = findViewById(R.id.delete_button);
 
@@ -36,7 +36,7 @@ public class UpdateActivity extends AppCompatActivity {
         //Set actionbar title after getAndSetIntentData method
         ActionBar ab = getSupportActionBar();
         if (ab != null) {
-            ab.setTitle(title);
+            ab.setTitle(payee);
         }
 
         update_button.setOnClickListener(new View.OnClickListener() {
@@ -44,10 +44,10 @@ public class UpdateActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //And only then we call this
                 MyDatabaseHelper myDB = new MyDatabaseHelper(UpdateActivity.this);
-                title = title_input.getText().toString().trim();
-                author = author_input.getText().toString().trim();
-                pages = pages_input.getText().toString().trim();
-                myDB.updateData(id, title, author, pages);
+                payee = payee_input.getText().toString().trim();
+                payer = payer_input.getText().toString().trim();
+                payment = payment_input.getText().toString().trim();
+                myDB.updateData(id, payee, payer, payment);
             }
         });
         delete_button.setOnClickListener(new View.OnClickListener() {
@@ -64,15 +64,15 @@ public class UpdateActivity extends AppCompatActivity {
                 getIntent().hasExtra("author") && getIntent().hasExtra("pages")){
             //Getting Data from Intent
             id = getIntent().getStringExtra("id");
-            title = getIntent().getStringExtra("title");
-            author = getIntent().getStringExtra("author");
-            pages = getIntent().getStringExtra("pages");
+            payee = getIntent().getStringExtra("title");
+            payer = getIntent().getStringExtra("author");
+            payment = getIntent().getStringExtra("pages");
 
             //Setting Intent Data
-            title_input.setText(title);
-            author_input.setText(author);
-            pages_input.setText(pages);
-            Log.d("stev", title+" "+author+" "+pages);
+            payee_input.setText(payee);
+            payer_input.setText(payer);
+            payment_input.setText(payment);
+            Log.d("stev", payee +" " + payer + " " + payment);
         }else{
             Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
         }
@@ -80,8 +80,8 @@ public class UpdateActivity extends AppCompatActivity {
 
     void confirmDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Delete " + title + " ?");
-        builder.setMessage("Are you sure you want to delete " + title + " ?");
+        builder.setTitle("Delete " + payee + " ?");
+        builder.setMessage("Are you sure you want to delete " + payee + " ?");
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
